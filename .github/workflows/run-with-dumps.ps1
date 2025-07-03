577cd2faa009ab7a39b0f5daa23991a56336afae
DaNike <danike@cirr.com>
2025-07-03 00:18:32 -0500

    Pass -S to sudo

--- .github/workflows/run-with-dumps.ps1
+++ .github/workflows/run-with-dumps.ps1
@@ -35,7 +35,7 @@ elseif ($IsLinux)
     $corePattern = Join-Path $dumpsPath "dump_%e_%p.core";
     Write-Output ($Args -join "`n") | bash -c @"
 set -eo pipefail;
-echo "$corePattern" | sudo tee /proc/sys/kernel/core_pattern;
+echo "\n$corePattern" | sudo -S tee /proc/sys/kernel/core_pattern;
 ulimit -c unlimited;
 ulimit -t 600; # hard-limit the program to take no more than 10 minutes (nothing we will use this for needs anywhere near that much; any more is a problem)
 set +e;
@@ -51,7 +51,7 @@ elseif ($IsMacOS)
     Write-Output ($Args -join "`n") | bash -c @"
 set -eo pipefail;
 sudo sysctl kern.coredump=1;
-sudo sysctl "kern.corefile=$corePattern";
+sysctl "kern.corefile=$corePattern";
 ulimit -c unlimited;
 ulimit -t 600; # hard-limit the program to take no more than 10 minutes (nothing we will use this for needs anywhere near that much; any more is a problem)
 set +e;
