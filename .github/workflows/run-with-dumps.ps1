5200889557aeba132f9421e4234fde0dbeb919af
DaNike <danike@cirr.com>
2025-08-02 19:02:14 -0500

    Deduplicate bash script for LLDB-based dumps

--- .github/workflows/run-with-dumps.ps1
+++ .github/workflows/run-with-dumps.ps1
@@ -36,7 +36,7 @@ if ($IsWindows)
     &$Exe @Args;
     exit $LastExitCode;
 }
-elseif ($IsLinux)
+elseif ($IsLinux -or $IsMacOS)
 {
     # on Linux, we need to set the core_pattern and run the app with a ulimit -c unlimited
     Write-Output ($Args -join "`n") | bash -c @"
@@ -44,6 +44,7 @@ set -eo pipefail;
 ulimit -c unlimited;
 ulimit -t 600; # hard-limit the program to take no more than 10 minutes (nothing we will use this for needs anywhere near that much; any more is a problem)
 set +e;
+# on MacOS, SIGXCPU doesn't coredump by default. Thus, we use LLDB unattended to perform the dump 
 # because we run our Linux stuff in containers, we can't set the core_pattern. Thus, we'll do the same thing we *must* do on MacOS and use LLDB to generate dumps when crashing
 xargs lldb -b -s "$(Join-Path $lldbHelpers 'setup.lldb')" \
     -k "process save-core -p minidump -s full -- '$(Join-Path $dumpsPath 'dump_crash.core')'" \
@@ -53,22 +54,6 @@ exit `$?;
 "@;
     exit $LastExitCode;
 
-}
-elseif ($IsMacOS)
-{
-    Write-Output ($Args -join "`n") | bash -c @"
-set -eo pipefail;
-ulimit -c unlimited;
-ulimit -t 600; # hard-limit the program to take no more than 10 minutes (nothing we will use this for needs anywhere near that much; any more is a problem)
-set +e;
-# on MacOS, SIGXCPU doesn't coredump by default. Thus, we use LLDB unattended to perform the dump 
-xargs lldb -b -s "$(Join-Path $lldbHelpers 'setup.lldb')" \
-    -k "process save-core -p minidump -s full -- '$(Join-Path $dumpsPath 'dump_crash.core')'" \
-    -K "$(Join-Path $lldbHelpers 'crash.lldb')" \
-    -s "$(Join-Path $lldbHelpers 'teardown.lldb')" -- "$Exe";
-exit `$?;
-"@;
-    exit $LastExitCode;
 }
 else
 {
